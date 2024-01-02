826f48cd5c7972b9f3273ce04a25b104cdd20117
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-02 16:40:22 -0300

    primeiro commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import Header from "../component/layout/header"
+import React from 'react'
+
+function App() {
+
+  return (
+   <main className="w-screen h-auto">
+   <Header/>
+
+   </main>
+  )
+}
+
+export default App
