426070929f84229a264772e8707b24e24f2bfb5c
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-04 01:44:36 -0300

    update

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -2,7 +2,7 @@ import LazyImage from "../../../components/LazyImage";
 import { CONSTANTS } from "../../../config/constants";
 
 const Logo = () => {
-    return <LazyImage src={CONSTANTS.logo} alt={"Blackconversa"} className="w-[350px] max-md:h-[35px] h-[30px] bg-cover bg-center bg-no-repeat bg-slate-500" />
+    return <LazyImage src={CONSTANTS.logo} alt={"Blackconversa"} className="w-[350px] max-md:h-[35px] h-[30px] bg-cover bg-center bg-no-repeat " />
 
 }
 
