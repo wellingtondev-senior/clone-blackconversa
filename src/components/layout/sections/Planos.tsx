f214e45b210d721da70d5645a78c38c8f9c73be7
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-05 20:50:09 -0300

    update

--- src/components/layout/sections/Planos.tsx
+++ src/components/layout/sections/Planos.tsx
@@ -4,7 +4,7 @@ interface PlanosProps {
 
 const Planos = ({id}:PlanosProps) => {
     return (
-        <section id={id} className="w-full h-screen bg-green-400">
+        <section id={id} className="w-full h-screen bg-[#0E0016]">
             Enter
         </section>
     );
