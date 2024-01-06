a76b7f0bf51e2716993c9e46399fdc42151d8e6c
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-06 18:22:08 -0300

    update

--- src/components/layout/sections/Planos.tsx
+++ src/components/layout/sections/Planos.tsx
@@ -8,11 +8,11 @@ interface PlanosProps {
 const Planos = ({ id }: PlanosProps) => {
     return (
         <section id={id} className="w-full min-h-screen bg-[#0E0016] ">
-            <article className="px-[100px] flex flex-col items-center justify-start pt-[100px] gap-4">
-                <Button className="cursor-default px-8 py-[20px] border border-[#3f2958] [background-image:radial-gradient(60%_50%_at_50%_50%,_#10011A_0%,_#17022D_100%)]" >
+            <article className="px-[100px] flex flex-col items-center justify-start pt-[100px] max-md:px-4 gap-4">
+                <Button className="cursor-default  py-[20px] border border-[#3f2958] [background-image:radial-gradient(60%_50%_at_50%_50%,_#10011A_0%,_#17022D_100%)]" >
                     Planos
                 </Button>
-                <h1 className="font-bold text-[45px] text-gray-100  text-center max-md:px-4 px-16">
+                <h1 className="w-full font-bold text-[45px] max-md:text-[30px] text-gray-100  text-center  px-16">
                     Conheça nossos planos
                 </h1>
                 <p className="text-gray-500 text-[15px] px-16  max-md:px-4">
--- src/components/palnos/index.tsx
+++ src/components/palnos/index.tsx
@@ -6,13 +6,19 @@ const PlanosComponent = () => {
 
     return (
         <article>
-             <div className="flex items-center justify-between w-[200px] h-[50px] p-[5px] rounded-full  bg-gradient-to-t from-[#180A1F] via-[#10011A] to-[#180A1F]">
-                    <Button onClick={()=> qtd == 1 ? setQtd(qtd):setQtd(qtd-1)} className="rounded-full w-[40px] h-[40px] text-[20px] text-gray-800 font-bold bg-gradient-to-r from-gray-50 to-[#A46FF3] flex items-center justify-center p-0">-</Button>
-                    <span className="text-[20px] text-gray-50 font-semibold">{qtd}</span>
-                    <Button onClick={()=>setQtd(qtd+1)} className="p-0 rounded-full w-[40px] h-[40px]  text-[20px] text-gray-100 font-bold flex items-center justify-center bg-gradient-to-r from-[#6127BC] to-[#A46FF3]">+</Button>
-                
+            <div className="flex items-center justify-between w-[200px] h-[50px] p-[5px] rounded-full  bg-gradient-to-t from-[#180A1F] via-[#10011A] to-[#180A1F]">
+                <Button onClick={() => qtd == 1 ? setQtd(qtd) : setQtd(qtd - 1)} className="rounded-full w-[40px] h-[40px] text-[20px] text-gray-800 font-bold bg-gradient-to-r from-gray-50 to-[#A46FF3] flex items-center justify-center p-0">-</Button>
+                <span className="text-[20px] text-gray-50 font-semibold">{qtd}</span>
+                <Button onClick={() => setQtd(qtd + 1)} className="p-0 rounded-full w-[40px] h-[40px]  text-[20px] text-gray-100 font-bold flex items-center justify-center bg-gradient-to-r from-[#6127BC] to-[#A46FF3]">+</Button>
+
+            </div>
+            <p className="text-gray-600 text-[14px] text-center">Número adicional: R$ 97,00</p>
+            <div>
+                <div >
+
                 </div>
-                <p className="text-gray-600 text-[14px] text-center">Número adicional: R$ 97,00</p>
+
+            </div>
         </article>
     );
 }
