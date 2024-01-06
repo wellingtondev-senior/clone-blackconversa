4095e6794b36e2b0cd23fcb38f9eda46ede4028e
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-06 19:13:36 -0300

    update

--- src/components/layout/sections/Planos.tsx
+++ src/components/layout/sections/Planos.tsx
@@ -8,14 +8,14 @@ interface PlanosProps {
 const Planos = ({ id }: PlanosProps) => {
     return (
         <section id={id} className="w-full min-h-screen bg-[#0E0016] ">
-            <article className="px-[100px] flex flex-col items-center justify-start pt-[100px] max-md:px-4 gap-4">
+            <article className="px-[100px] flex flex-col items-center justify-start pt-[100px] pb-[100px] max-md:px-4 gap-4">
                 <Button className="cursor-default  py-[20px] border border-[#3f2958] [background-image:radial-gradient(60%_50%_at_50%_50%,_#10011A_0%,_#17022D_100%)]" >
                     Planos
                 </Button>
                 <h1 className="w-full font-bold text-[45px] max-md:text-[30px] text-gray-100  text-center  px-16">
                     Conheça nossos planos
                 </h1>
-                <p className="text-gray-500 text-[15px] px-16  max-md:px-4">
+                <p className="text-gray-500 text-[15px] text-center px-16  max-md:px-4">
                     Quantos números de Whatsapp você precisa conectar?
                 </p>
                <PlanosComponent/>
--- src/components/palnos/index.tsx
+++ src/components/palnos/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Button } from "../../components/ui/button";
+import LazyImage from "../../components/LazyImage";
+import { PLANOS } from "../../config/planos";
 
 const PlanosComponent = () => {
     const [qtd, setQtd] = useState<number>(1);
 
     return (
-        <article>
+        <article className="flex flex-col items-center justify-start">
             <div className="flex items-center justify-between w-[200px] h-[50px] p-[5px] rounded-full  bg-gradient-to-t from-[#180A1F] via-[#10011A] to-[#180A1F]">
                 <Button onClick={() => qtd == 1 ? setQtd(qtd) : setQtd(qtd - 1)} className="rounded-full w-[40px] h-[40px] text-[20px] text-gray-800 font-bold bg-gradient-to-r from-gray-50 to-[#A46FF3] flex items-center justify-center p-0">-</Button>
                 <span className="text-[20px] text-gray-50 font-semibold">{qtd}</span>
@@ -14,10 +16,28 @@ const PlanosComponent = () => {
             </div>
             <p className="text-gray-600 text-[14px] text-center">Número adicional: R$ 97,00</p>
             <div>
-                <div >
+                <div className="pt-10">
+                    <div className="w-[380px] p-8 bg-[#1B1026] rounded-lg border border-[#21132E]">
+
+                        <div className="flex items-center justify-start gap-4">
+                            <LazyImage src={PLANOS[0].icon} alt="background" imageSize="100%" className=" w-[40px] h-[40px]  bg-contain bg-no-repeat bg-left" />
+                            <span className="text-[40px] font-semibold text-gray-50">STARTER</span>
+                        </div>
+                        <p className="text-left text-[14px] text-gray-600">Perfeito para quem esta iniciando</p>
+                        <div className="flex items-center justify-start gap-4 pt-16 pb-2">
+                            <p className="text-[18px] text-gray-50">De</p>
+                            <p className="line-through text-[18px] text-red-700 font-semibold">R$ 147,00</p>
+                            <p className="text-[18px] text-gray-50"> por:</p>
+                        </div>
+                        <div className="flex items-end justify-start gap-4">
+                            <span className="text-[30px] text-gray-50">R$
+                                <span className="text-[50px] text-gray-50 font-semibold p-0 mx-2">97,00</span>
+                                <span className=" text-[18px] text-gray-500"> /Primeiro Mês</span>
+                            </span>
+                        </div>
+                    </div>
 
                 </div>
-
             </div>
         </article>
     );
