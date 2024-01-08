ab4cd0a49a12784b705798f96daf5ade6e075501
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-08 14:49:15 -0300

    update

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -6,13 +6,15 @@ const Footer = () => {
         <section className="w-full bg-[#0E0016] h-auto py-[100px] px-20 max-md:px-6">
             <article className="border-t border-gray-700 w-full ">
                 <LazyImage src={CONSTANTS.logo} alt="background" className="absolute max-md:mt-[-25px] mt-[-35px] bg-[#0E0016] w-[350px] max-md:w-[200px] h-[80px]  bg-contain bg-no-repeat bg-top" />
-              <div className="flex">
-                <div>
-                    <span>BlackConversa é o seu construtor de fluxos, criado para você e por você</span>
+                <div className="flex">
+                    <div className="w-[30%]">
+                        <span>BlackConversa é o seu construtor de fluxos, criado para você e por você</span>
+                    </div>
+                    <div className="w-[70%]">
+
+                    </div>
                 </div>
-                <div></div>
-              </div>
-              </article>
+            </article>
 
         </section>
     );
