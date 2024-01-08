499e574f0805bea7ade32c05acd320e0109288d9
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-08 16:24:25 -0300

    update

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -48,7 +48,6 @@ const Footer = () => {
                         </ul>
                         <div className="text-gray-50 flex flex-col items-end mt-10 max-md:items-center">
                             <span>Blackzera © 2023 - Todos os direitos reservados</span>
-                            <p>Desenvolvido por <span className="text-[11px] text-[#6326B9]">GOAT Studio Design</span></p>
                         </div>
                     </div>
                 </div>
--- src/components/palnos/index.tsx
+++ src/components/palnos/index.tsx
@@ -76,7 +76,7 @@ const PlanosComponent = () => {
             <div className="flex items-center justify-between w-[200px] h-[50px] p-[5px] rounded-full  bg-gradient-to-t from-[#180A1F] via-[#10011A] to-[#180A1F]">
                 <Button onClick={() => qtd == 1 ? setQtd(qtd) : setQtd(qtd - 1)} className="rounded-full w-[40px] h-[40px] text-[20px] text-gray-800 font-bold bg-gradient-to-r from-gray-50 to-[#A46FF3] flex items-center justify-center p-0">-</Button>
                 <span className="text-[20px] text-gray-50 font-semibold">{qtd}</span>
-                <Button onClick={() => setQtd(qtd + 1)} className="p-0 rounded-full w-[40px] h-[40px]  text-[20px] text-gray-100 font-bold flex items-center justify-center bg-gradient-to-r from-[#6127BC] to-[#A46FF3]">+</Button>
+                <Button onClick={() => qtd < 10 ? setQtd(qtd + 1):setQtd(qtd)} className="p-0 rounded-full w-[40px] h-[40px]  text-[20px] text-gray-100 font-bold flex items-center justify-center bg-gradient-to-r from-[#6127BC] to-[#A46FF3]">+</Button>
 
             </div>
             <p className="text-gray-600 text-[14px] text-center">Número adicional: R$ 97,00</p>
@@ -172,7 +172,7 @@ const PlanosComponent = () => {
                 <Button onClick={setValorMensal} className={mensal ? swift.mensal.active : swift.mensal.noactive}>Mensal</Button>
                 <Button onClick={setValorMensal} className={mensal ? swift.anual.noactive : swift.anual.active}>Anual</Button>
             </div>
-            <span className="text-gray-600 text-[20px] text-center mt-10 ">Você economiza R$596 no Plano Anual.</span>
+            <span className="text-gray-100 text-[20px] text-center mt-10 ">Você economiza R$596 no Plano Anual.</span>
 
         </article>
     );
