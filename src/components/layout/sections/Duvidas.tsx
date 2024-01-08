f8eb3f47838c2fb91dc2711f16545893f1603365
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-01-08 14:14:58 -0300

    update

--- src/components/layout/sections/Duvidas.tsx
+++ src/components/layout/sections/Duvidas.tsx
@@ -1,11 +1,16 @@
+import DuvidasAcordion from "../../../components/DuvidasAcordion";
+
 interface DuvidasProps {
-    id:string
+    id: string
 }
 
-const Duvidas = ({id}:DuvidasProps) => {
+const Duvidas = ({ id }: DuvidasProps) => {
     return (
-        <section id={id} className="w-full h-screen bg-[#0E0016]">
-            Enter
+        <section id={id} className="px-36 py-[100px] max-md:px-4 w-full min-h-screen h-auto bg-[#0E0016] flex flex-col items-center justify-center gap-6">
+            <span className="text-gray-50 text-[40px] font-semibold ">
+                Perguntas frequentes
+            </span>
+            < DuvidasAcordion/>
         </section>
     );
 }
